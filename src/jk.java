c7c1f2648281bdd5ab9b3a794d1eede8e1079789
harshvardhanprajapati <harshvardhanprajapati10@gmail.com>
2022-09-16 23:33:48 +0530

    initial commite

---
+++ src/jk.java
@@ -0,0 +1,9 @@
+public class jk {
+    public static void main(String[] args) {
+        int a = 56;
+        while (a < 78) {
+            System.out.println("hello ");
+            a++;
+        }
+    }
+}
