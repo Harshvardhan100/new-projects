3fcfdca7eb68d3d171a9a9a3d422a1e7a8e8905e
Harshvardhan Prajapati <105856460+Harshvardhan100@users.noreply.github.com>
2022-10-24 23:34:11 +0530

    This keyword uses

---
+++ this_keyword.java
@@ -0,0 +1,21 @@
+class dp {
+    int a;
+
+
+    dp(int v) {
+        this.a = v;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+}
+
+public class this_and_super_keyword {
+    public static void main(String[] args) {
+
+dp cd=new dp(8);
+        System.out.println(cd.getA());
+    }
+}
