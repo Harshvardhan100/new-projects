ab6bd543386789d1ebad68dbca10f264129683b6
Harshvardhan Prajapati <105856460+Harshvardhan100@users.noreply.github.com>
2022-11-03 23:32:01 +0530

    Create The_new_hackerrank234.java

---
+++ The_new_hackerrank234.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class import java.util.*;
+public class The_new_hackerrank234 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+
+       for(int i=0;i<4;i++){
+           String e=sc.nextLine();
+           int r=sc.nextInt();
+           System.out.println(e);
+           System.out.println(r);
+       }
+
+
+
+        System.out.println("================================");
+    }
+}
+ {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+
+       for(int i=0;i<4;i++){
+           String e=sc.nextLine();
+           int r=sc.nextInt();
+           System.out.println(e);
+           System.out.println(r);
+       }
+
+
+
+        System.out.println("================================");
+    }
+}
